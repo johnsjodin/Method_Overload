730a42dc68a5776edb7849003774884bb826eb62
John Sjödin <sjodin.j@gmail.com>
2025-09-17 10:41:50 +0200

    Add overloaded methods from exercise.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FormatName("Anna", "Svensson"));
+            Console.WriteLine(FormatName("Anna", "Maria", "Svensson"));
+            Console.WriteLine(FormatName("Anna", "Svensson", true));
+        }
+        static string FormatName(string first, string last)
+        {
+            return $"{first} {last}";
+        }
+
+        static string FormatName(string first, string middle, string last)
+        {
+            return $"{first} {middle} {last}";
+        }
+
+        static string FormatName(string first, string last, bool uppercase)
+        {
+            var full = $"{first} {last}";
+            return uppercase ? full.ToUpper() : full;
         }
     }
 }
