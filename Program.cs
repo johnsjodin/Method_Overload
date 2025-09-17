a7b2f3d3adf25779f45775946d84dd4c4366c29b
John Sjödin <sjodin.j@gmail.com>
2025-09-17 10:51:44 +0200

    Add comment.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 {
     internal class Program
     {
+        /*      Varför är detta överlagring?
+         * Det är överlagring för att det finns flera metoder med samma namn (FormatName)
+         * som tar emot olika typer eller antal argument.
+         * 
+         *      Hur gör detta koden lättare att läsa & återanvända?
+         * I och med att det är samma namn på metoderna så behöver man inte skriva olika
+         * metodnamn på andra ställen i koden för att formatera namnen på olika sätt, och
+         * om man behöver formattera namnet på ett nytt sätt så är det bara att lägga till
+         * en metod med samma namn och man behöver inte ändra något annat i koden.
+        */
+
         static void Main(string[] args)
         {
             Console.WriteLine(FormatName("Anna", "Svensson"));
